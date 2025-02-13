4ee1caf33440a72f73d157637fc5a4eb10a58ac9
HUSNEJIDAN <119681826+HUSNEJIDAN@users.noreply.github.com>
2025-02-13 14:18:26 +0600

    complete api project

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-const loadPhone = async (searchText) =>{
+const loadPhone = async (searchText='iphone',isShowAll) =>{
   const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
   const data = await res.json();
   const phones = data.data;
-  console.log(phones);
+  //console.log(phones);
   // console.log(phones);
-  displayPhones(phones);
+  displayPhones(phones,isShowAll);
 }
 
-const displayPhones = phones =>{
+const displayPhones = (phones,isShowAll) =>{
   // console.log(phones)
   //1: where you want set...
 
@@ -19,14 +19,16 @@ const displayPhones = phones =>{
 
   //set how many output you want and after that left of the element will appear by click on show more button
   const showAllContainer = document.getElementById('show-more-container')
-  if(phones.length > 12){
+  if(phones.length > 12 && !isShowAll){
       showAllContainer.classList.remove('hidden')
   }
   else{
       showAllContainer.classList.add('hidden');
    }
 
-  phones = phones.slice(0,12)
+  if(!isShowAll){
+    phones = phones.slice(0,12)
+  }
 
   phones.forEach(phone =>{
       //console.log(phone);
@@ -38,9 +40,9 @@ const displayPhones = phones =>{
       <figure><img src="${phone.image}" alt="Shoes" /></figure>
       <div class="card-body">
           <h2 class="card-title">${phone.phone_name}</h2>
-          <p>${phone.slug}</p>
+          <p></p>
           <div class="card-actions justify-end">
-              <button class="btn btn-primary">Buy Now</button>
+              <button onclick="handleShowDetail('${phone.slug}')"class = "btn btn-primary"> Show details </button>
           </div>
       </div>
       `;
@@ -49,10 +51,43 @@ const displayPhones = phones =>{
   })
 }
 
-const handleSearch = () =>{
+const handleSearch = (isShowAll) =>{
   //console.log('search clicked')
   const searchField = document.getElementById('search-field').value;
   //console.log(searchField)
-  loadPhone(searchField);
+  loadPhone(searchField, isShowAll);
 }
 
+//handle see all button
+
+const handleSeeAll = () => {
+  //console.log('see all clicked')
+  handleSearch(true);
+}
+
+const handleShowDetail = async (id) => {
+    //console.log("show detail click")
+
+    //load single phone data
+   const res = await fetch (` https://openapi.programming-hero.com/api/phone/${id}`)
+   const data = await res.json();
+   const phone = data.data;
+   showPhoneDetail(phone)
+
+}
+ const showPhoneDetail = (phone) => {
+  console.log(phone);
+  show_detail_modal.showModal();
+  const phoneName = document.getElementById('shoe-detail-phone-name');
+  phoneName.innerText = phone.name;
+  const showDetailContainer = document.getElementById('show-detail-container')
+  showDetailContainer.innerHTML = `
+    <img src="${phone.image}"/>
+    <p> Storage: "${phone.mainFeatures
+      .storage}/p>
+      <p> Brand: "${phone.brand}"</p>
+      <p> releaseDate: "${phone.releaseDate}"</p>
+  `
+ }
+
+loadPhone();
